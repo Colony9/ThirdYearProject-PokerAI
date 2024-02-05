77b5eae29c4c400fa5255e663fd1cb46878cc465
Charlie Kearton <ed20ck@leeds.ac.uk>
2024-02-05 12:11:40 +0000

    Added descriptions for each unit test

--- tests/PlayerTests.py
+++ tests/PlayerTests.py
@@ -9,40 +9,53 @@ class testPlayer(unittest.TestCase):
     def setUp(self):
         self.player = Player("The Tester", 500)
     
+    #This test checks that each attribute of the player is assigned correctly.
     def testConstruction(self):
         self.assertEqual(self.player.name, "The Tester")
         self.assertEqual(self.player.chips, 500)
         self.assertEqual(self.player.bet, 0)
         self.assertFalse(self.player.folded)
     
+    #This test checks that the call method assigns the player's bet correctly.
     def testBasicCall(self):
         self.player.playCall(100)
         self.assertEqual(self.player.bet, 100)
 
+    #This test checks that the call method assigns correctly, unaffected by
+    #previous player choices.
     def testSuccessiveCall(self):
         self.player.playCall(100)
         self.assertEqual(self.player.bet, 100)
         self.player.playCall(200)
         self.assertEqual(self.player.bet, 200)
     
+    #This test checks that a call made with a wager greater than the player's
+    #current chips results in them going all in - with the correct bet value.
     def testAllInCall(self):
         self.player.playCall(1000)
-        self.assertEqual(self.player.bet, 500)
+        self.assertEqual(self.player.bet, self.player.chips)
     
+    #This test checks that the raise methods assigns the player's bet correctly.
     def testBasicRaise(self):
         self.player.playRaise(200)
         self.assertEqual(self.player.bet, 200)
     
+    #This test checks that the raise methods assigns (rather than adds) 
+    #correctly when performed after a previous raise.
     def testSuccessiveRaise(self):
         self.player.playRaise(100)
         self.assertEqual(self.player.bet, 100)
         self.player.playRaise(200)
         self.assertEqual(self.player.bet, 200)
     
+    #This test checks that a raise greater than the player's chips correctly
+    #results in them going all in.
     def testAllInRaise(self):
         self.player.playRaise(1000)
-        self.assertEqual(self.player.bet, 500)
+        self.assertEqual(self.player.bet, self.player.chips)
     
+    #This test checks that the fold methods correctly assigns the player's
+    #'folded' status as true.
     def testFold(self):
         self.player.playFold()
         self.assertTrue(self.player.folded)
--- tests/RoundTests.py
+++ tests/RoundTests.py
@@ -12,6 +12,9 @@ class testRound(unittest.TestCase):
         self.player1 = Player("First Player", 500)
         self.player2 = Player("Second Player", 1000)
     
+    #This test checks that the 'dealPlayer' method correctly assigns cards from
+    #the deck to the player's pocket and moves the 'deck_top' marker forward by
+    #the correct amount.
     def testDealPlayer(self):
         self.round.dealPlayer(self.player1, 3)
         self.assertEqual(len(self.player1.pocket), 3)
@@ -21,6 +24,8 @@ class testRound(unittest.TestCase):
         self.assertEqual(len(self.player2.pocket), 3)
         self.assertEqual(self.round.deck_top, 6)
     
+    #This test checks that the community cards can be dealt correctly, similar
+    #to the pocket cards.
     def testDealCommunity(self):
         self.round.dealCommunity(3)
         self.assertEqual(len(self.round.community_cards), 3)
@@ -33,7 +38,9 @@ class testRound(unittest.TestCase):
         
         self.assertEqual(len(self.round.community_cards), 5)
         self.assertEqual(self.round.deck_top, 5)
-        
+    
+    #This test checks that the dealing methods will return without doing
+    #anything if given a negative number of cards to deal.
     def testDealNegative(self):
         self.round.dealPlayer(self.player1, -2)
         self.assertEqual(len(self.player1.pocket), 0)
@@ -43,6 +50,9 @@ class testRound(unittest.TestCase):
         self.assertEqual(len(self.round.community_cards), 0)
         self.assertEqual(self.round.deck_top, 0)
     
+    #This test simulates a simple betting round before collecting the bets,
+    #determining whether the pot's value is calculated correctly and that
+    #chips are successfully deducted from the players.
     def testCollectBets(self):
         self.player1.playRaise(200)
         self.player2.playCall(200)
@@ -53,12 +63,16 @@ class testRound(unittest.TestCase):
         self.assertEqual(self.player1.chips, 300)
         self.assertEqual(self.player2.chips, 800)
     
+    #This test checks that the 'collectBets' method treats a negative-value bet
+    #as equal to 0.
     def testCollectBetsNegative(self):
         self.player1.bet = -100
         self.player2.bet = 100
         self.round.collectBets([self.player1, self.player2])
         self.assertEqual(self.round.pot, 100)
     
+    #This test checks that the 'payout' method correctly adds the pot's value
+    #to the winning player's chips before setting the pot to 0.
     def testPayout(self):
         self.round.pot = 400
         self.round.payout(self.player1)
