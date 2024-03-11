9b73407fdde21af4838ee3731a5f7c88349dfec1
Charlie Kearton <ed20ck@leeds.ac.uk>
2024-03-11 11:59:41 +0000

    Create Basis for Counterfactual Regreat Minimisation AI

---
+++ AI_Players/OpponentProfile.py
@@ -0,0 +1,28 @@
+
+class opponentProfile():
+    def __init__(self):
+        #The 'raise_rate' attribute measures how likely it is that the opponent 
+        #will raise, given that the wager value is a certain portion of their chips
+        self.raise_rate = [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.1, 0.1, 0.0, 0.0]
+        self.raise_coefficients = [0, 0, 0]
+        #The 'fold_rate' attribute measures how likely it is that the opponent
+        #will fold, given that the wager value is a certain portion of their chips
+        self.fold_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.9]
+        self.fold_coefficients = [0, 0, 0]
+        
+        #The 'call_rate' attribute measures how likely it is that the opponent
+        #will call/check, given that the wager value is a certain portion of their chips
+        self.call_rates = [0.8, 0.7, 0.6, 0.4, 0.3, 0.2, 0.1, 0.1, 0.1, 0.1, 0.1]
+        self.call_coefficients = [0, 0, 0]
+        
+        self.chip_percentage = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+        self.game_marker = 2
+        
+    def getRaiseRate(self):
+        pass
+    
+    def getFoldRate(self):
+        pass
+    
+    def getCallRate(self):
+        pass
---
+++ AI_Players/SearchTree.py
@@ -0,0 +1,53 @@
+
+class treeNode():
+    def __init__(self, identity, action_route, odds, pot_value, parent=None):
+        self.identity = identity
+        self.action_route = action_route
+        self.value = None
+        self.parent = parent
+        self.children = []
+
+        self.odds = None
+        self.regret_values = [0, 0, 0, 0, 0]
+        self.pot_val = pot_value
+
+    def expandChildren(self):
+        pass
+
+    def backPropagate(self):
+        if self.identity == "player":
+            self.calculateRegret()
+        if self.identity == "opponent":
+            self.value
+            for child in self.children:
+                self.value += child.odds * child.value
+        return
+            
+
+    def calculateRegret(self):
+        if self.identity != "player":
+            return
+
+        regret_sum = 0
+        for child in self.children:
+            regret_sum += child.odds * child.value
+
+        self.value = regret_sum
+        for c in range(len(self.children)):
+            self.regret_values[c] += max(0, self.children[c].value - self.value)
+        return
+
+    def readjustOdds(self):
+        if self.identity == "player":
+            normalisation_factor = 0
+            for regret in self.regret_values:
+                normalisation_factor += regret
+            
+            for c in range(len(self.children)):
+                self.children[c].odds = self.regret_values[c] / normalisation_factor
+
+        elif self.identity == "opponent":
+            pass
+
+def completeSubTree(root_node, hand_name, depth_limit):
+    pass
